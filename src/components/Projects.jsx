a1a54faee844bb80379a3ca18fbe8e066d53a6d2
Aly <el7amrawy@outlook.com>
2022-09-21 22:26:46 +0200

    added new frontend projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,9 @@ import landingPage from "../assets/images/landing-page.png";
 import newsAPI from "../assets/images/news-api.png";
 import weather from "../assets/images/weather.png";
 import ts from "../assets/images/ts_logo.png";
+import notes from "../assets/images/notes.png";
+import quizzical from "../assets/images/quizzical.jpg";
+import tenzies from "../assets/images/tenzies.png";
 //
 
 export default function Projects() {
@@ -39,9 +42,24 @@ export default function Projects() {
             url="https://el7amrawy.github.io/timezone-temp/"
           />
           <Project
-            name="Landing Page"
-            img={landingPage}
-            url="https://el7amrawy.github.io/Landing-Page/"
+            name="Notes App"
+            img={notes}
+            url="https://github.com/el7amrawy/notes-app"
+          />
+          <Project
+            name="Quizzical"
+            img={quizzical}
+            url="https://el7amrawy.github.io/quizzical"
+          />
+          <Project
+            name="Tenzies"
+            img={tenzies}
+            url="https://el7amrawy.github.io/tenzies"
+          />
+          <Project
+            name="Weather-Journal App"
+            img={weather}
+            url="https://github.com/el7amrawy/Weather-Journal-App"
           />
           <Project
             name="News App"
@@ -49,9 +67,9 @@ export default function Projects() {
             url="https://github.com/el7amrawy/news-api"
           />
           <Project
-            name="Weather-Journal App"
-            img={weather}
-            url="https://github.com/el7amrawy/Weather-Journal-App"
+            name="Landing Page"
+            img={landingPage}
+            url="https://el7amrawy.github.io/Landing-Page/"
           />
         </div>
       ) : (
